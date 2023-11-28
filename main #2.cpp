845c35302150898c0b60ae98b32d8a5aabecedcc
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2023-11-27 20:40:27 -0500

    Add files via upload

---
+++ main #2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello World"  <<  "Jahim" << endl;
+    cout<<"Hello World"  <<  "Sankoh";
+    return 0;
+}
---
+++ main #3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  int x, y;
+  string x;  
+  cout << x; 
+}
