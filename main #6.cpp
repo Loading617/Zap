08bf74ff3225f9ade384b9b08c2094450fff1884
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2023-12-13 08:22:22 -0500

    Add files via upload

---
+++ main #6.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+main () {
+int num1, num2;
+cout << "Type a number: ";
+cin >> num1;
+cout << "Type a number: ";
+cin >> num2;
+cout << "These two numbers are the same: " << (num1 == num2);
+}
+/*
+<
+>
+<=
+>=
+!=
+==
+*/
